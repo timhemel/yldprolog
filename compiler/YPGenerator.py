ee10de684ab3390248e3ee5c56e9d5f9f71c3eee
Tim Hemel <tim@localhost.localdomain>
2019-09-24 18:48:52 +0200

    add license information

--- compiler/YPGenerator.py
+++ compiler/YPGenerator.py
@@ -1,3 +1,25 @@
+# yldprolog - a rewrite of Yield Prolog
+#
+# This file is part of yldprolog.
+#
+# yldprolog is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License,
+# version 3, as published by the Free Software Foundation.
+#
+# yldprolog is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License, version 3, along with BANG.  If not, see
+# <http://www.gnu.org/licenses/>
+#
+# Copyright 2018-2019 - Tim Hemel
+# Licensed under the terms of the GNU Affero General Public License
+# version 3
+# SPDX-License-Identifier: AGPL-3.0-only
+
 
 from YPPrologVisitor import *
 
