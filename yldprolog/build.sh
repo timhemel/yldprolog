bc141e6a1d829b8624730a70456dfa338b732264
Tim Hemel <tim@localhost.localdomain>
2019-09-25 12:41:50 +0200

    Generate prolog compiler for python3

--- yldprolog/build.sh
+++ yldprolog/build.sh
@@ -4,5 +4,5 @@ GRAMMAR=prolog
 
 rm -f ${GRAMMAR}Lexer.py ${GRAMMAR}Lexer.tokens ${GRAMMAR}Listener.py ${GRAMMAR}Parser.py ${GRAMMAR}.tokens ${GRAMMAR}Visitor.py
 
-antlr4 -Dlanguage=Python2 -visitor ${GRAMMAR}.g4
+antlr4 -Dlanguage=Python3 -visitor ${GRAMMAR}.g4
 
