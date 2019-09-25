5f00d6550b94d1ed5ec15d8b14e333217670d25b
Tim Hemel <tim@localhost.localdomain>
2019-09-25 11:34:38 +0200

    First attempt at setup script

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="yldprolog",
+    version="0.1.0",
+    description='A rewrite of Yield Prolog for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Tim Hemel",
+    author_email="yldprolog@timit.nl",
+    # url='TODO',
+    # project_urls= { },
+    license='GNU Affero General Public License v3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent"
+    ],
+    keywords=['prolog'],
+    packages=setuptools.find_packages(include=['compiler'], exclude=['test']),
+    py_modules=['yldprolog'],
+    entry_points={
+        'console_scripts': ['yldpc=compiler:compile']
+    },
+    python_requires='>=3.6',
+)
+
