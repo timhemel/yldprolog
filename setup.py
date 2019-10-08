a422a0554b3afc0f5370441739a961b6509b4d75
Tim Hemel <tim@localhost.localdomain>
2019-10-08 19:51:22 +0200

    bump version for testing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name="yldprolog",
-    version="0.1.0",
+    version="0.1.1",
     description='A rewrite of Yield Prolog for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
