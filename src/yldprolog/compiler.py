9760fe8a188b2b5d82ef85b3f9ed2b222ca3056a
Tim Hemel <tim@securesoftware.nl>
2021-04-23 19:16:09 +0200

    Add documentation to define compiler module API.

--- src/yldprolog/compiler.py
+++ src/yldprolog/compiler.py
@@ -34,7 +34,8 @@ import contextlib
 import click
 from .errors import ParseError
 
-def compile_prolog_from_stream(inp, ctx):
+def _compile_prolog_from_stream(inp, ctx):
+    '''compiles prolog source from an antlr4 stream.'''
     lexer = prologLexer(inp)
     stream = CommonTokenStream(lexer)
     parser = prologParser(stream)
@@ -47,21 +48,45 @@ def compile_prolog_from_stream(inp, ctx):
     pythoncode = generator.generate(code)
     return pythoncode
 
-class DummyContext:
+class CompilerContext:
+    '''This class defines properties that affect the behaviour of the
+    compiler.
+
+    General options:
+    ----------------
+
+    current_source_file: filename of the input source. Used in error messages
+                         and debug output.
+
+    Debugging options:
+    ------------------
+
+    debug_filename:  show the filename in the generated code.
+    debug_parser:    enable debug output for the parser (visitor).
+    debug_generator: enable debug output for the code generator.
+    outf:            output file object
+    '''
+
     debug_filename = ''
     debug_parser = False
     debug_generator = False
+    current_source_file = ''
+    outf = None
 
-def compile_prolog_from_string(source, options=DummyContext):
+def compile_prolog_from_string(source, options=CompilerContext):
+    '''compiles prolog from the string source. options is an object or class
+    that defines the properties as specified in CompilerContext.'''
     inp = antlr4.InputStream(source)
-    return compile_prolog_from_stream(inp, options)
+    return _compile_prolog_from_stream(inp, options)
 
-def compile_prolog_from_file(path, options=DummyContext):
+def compile_prolog_from_file(path, options=CompilerContext):
+    '''compiles prolog from the string source. options is an object or class
+    that defines the properties as specified in CompilerContext.'''
     inp = FileStream(path, encoding='utf8')
-    return compile_prolog_from_stream(inp, options)
+    return _compile_prolog_from_stream(inp, options)
 
 @contextlib.contextmanager
-def open_input_file(fn):
+def _open_input_file(fn):
     if fn == '-':
         inf = StdinStream()
     else:
@@ -72,7 +97,7 @@ def open_input_file(fn):
         del inf
 
 @contextlib.contextmanager
-def open_output_file(fn):
+def _open_output_file(fn):
     if fn == '-':
         outf = sys.stdout
     else:
@@ -82,7 +107,7 @@ def open_output_file(fn):
     finally:
         outf.close()
 
-def set_debug_options(ctx):
+def _set_debug_options(ctx):
     ctx.debug_parser = ctx.params['debug_parser']
     ctx.debug_generator = ctx.params['debug_generator']
     ctx.debug_filename = ctx.params['debug_filename']
@@ -92,9 +117,9 @@ def set_debug_options(ctx):
         ctx.debug_filename = True
 
 
-CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+_CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
-@click.command(context_settings=CONTEXT_SETTINGS)
+@click.command(context_settings=_CONTEXT_SETTINGS)
 @click.option('-d','--debug',is_flag=True)
 @click.option('-o','--outfile',type=click.Path(allow_dash=True), default='-')
 @click.option('--debug-parser', is_flag=True, default=False)
@@ -103,17 +128,17 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.argument('source', nargs=-1, type=click.Path(exists=True,file_okay=True,dir_okay=False,readable=True,allow_dash=True))
 @click.pass_context
 def main(ctx, source, outfile, debug, debug_parser, debug_generator, debug_filename):
+    """Run the compiler CLI."""
 
-    set_debug_options(ctx)
+    _set_debug_options(ctx)
 
-    with open_output_file(outfile) as outf:
+    with _open_output_file(outfile) as outf:
         ctx.outf = outf
-        # TODO: multiple source files
         for s in source:
             ctx.current_source_file = s
-            with open_input_file(s) as inf:
+            with _open_input_file(s) as inf:
                 try:
-                    pythoncode = compile_prolog_from_stream(inf, ctx)
+                    pythoncode = _compile_prolog_from_stream(inf, ctx)
                     outf.write(pythoncode)
                 except ParseError as e:
                     raise click.ClickException(str(e)) from e
